f7bb2738f0bb4300e2c1f258a1c466817d00f294
Erick Otenyo <otenyo.erick@gmail.com>
2021-02-25 21:44:22 +0300

    Add Dockerfile

---
+++ docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ "$DATABASE" = "postgres" ]; then
+  echo "Waiting for postgres..."
+
+  while ! nc -z $SQL_HOST $SQL_PORT; do
+    sleep 0.1
+  done
+
+  echo "PostgreSQL started"
+fi
+# setup database
+flask setup_db
+
+# Migrate db
+flask db upgrade
+
+exec "$@"
