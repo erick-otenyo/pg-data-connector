77afdeedc0c72d219436e88e6d3f117d3e06810d
Erick Otenyo <otenyo.erick@gmail.com>
2022-05-24 16:52:18 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,9 @@
 #!/bin/sh
-if [ "$DB" = "postgres" ]; then
-  echo "Waiting for postgres..."
 
-  while ! nc -z $DB_HOST $DB_PORT; do
-    sleep 0.1
-  done
+# setup database
+flask setup_db
 
-  echo "PostgreSQL started"
+# Migrate db
+flask db upgrade
 
-  # setup database
-  flask setup_db
-
-  # Migrate db
-  flask db upgrade
-
-  exec "$@"
-fi
+exec "$@"
